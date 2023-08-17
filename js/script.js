253c56bddf77ca3c81192fa9537975acd14b95f9
fernandakaory <rm551104@fiap.com.br>
2023-08-17 19:28:48 -0300

    arrumando valor e duração

--- js/script.js
+++ js/script.js
@@ -19,7 +19,8 @@ btnAddTarefa.addEventListener("click",(evento)=>{
         autor: autor.value,
         departamento: departamento.value,
         importancia: importancia.value,
-        valor : valor.value
+        valor: null, // Definindo o valor como nulo
+        duracao: null, // Definindo a duração como nula
     };
 
     arrayTarefas.push(novaTarefa)
@@ -32,7 +33,8 @@ btnAddTarefa.addEventListener("click",(evento)=>{
 
 const btnOrdenar = document.getElementById("btnOrdenar");
 
-btnOrdenar.addEventListener("click", () => {
+btnOrdenar.addEventListener("click", (evento) => {
+    evento.preventDefault();
     ordenarPorImportancia(arrayTarefas);
     criaCelula(arrayTarefas);
 });
@@ -40,7 +42,7 @@ btnOrdenar.addEventListener("click", () => {
 
 function ordenarPorImportancia(array) {
     array.sort((a, b) => {
-        return a.importancia - b.importancia;
+        return b.importancia - a.importancia;
     });
 }
 
@@ -51,46 +53,88 @@ function criaCelula(arrayTarefas) {
     //o objeto e seu indice como parametro. cada objeto recebe um indeice
     arrayTarefas.forEach((objeto, index) => {
         let novaLinha = document.createElement("tr")
-        let btnExcluir = document.createElement("button");
-        btnExcluir.textContent = "Excluir"
+        // let btnExcluir = document.createElement("button");
+        // btnExcluir.textContent = "Excluir"
         let btnConcluir = document.createElement("button");
         btnConcluir.textContent = "Concluído"
         let btnValor = document.createElement("button");
         btnValor.textContent = "Valor"
+        let btnDuracao = document.createElement("button");
+        btnDuracao.textContent = "Duração"
 
         //usando o append direto, sem declarar uma variável para o valor 
         novaLinha.appendChild(criaElemento(objeto.descricao,"td"));
         novaLinha.appendChild(criaElemento(objeto.autor,"td"));
         novaLinha.appendChild(criaElemento(objeto.departamento,"td"));
         novaLinha.appendChild(criaElemento(objeto.importancia,"td"));
+
+           // Verificar se o objeto da tarefa já possui um valor para 'valor'
+           if (objeto.valor) {
+            // Criar uma célula adicional para exibir o valor
+            let valorCelula = document.createElement("td");
+            valorCelula.textContent = `Valor: ${objeto.valor}`;
+            novaLinha.appendChild(valorCelula);
+        }
+        
+        // Verificar se o objeto da tarefa já possui um valor para 'duracao'
+        if (objeto.duracao) {
+            // Criar uma célula adicional para exibir a duração
+            let duracaoCelula = document.createElement("td");
+            duracaoCelula.textContent = `Duração: ${objeto.duracao}`;
+            novaLinha.appendChild(duracaoCelula);
+        }
         // novaLinha.appendChild(criaElemento("Excluir","button"))
-        novaLinha.appendChild(btnExcluir);
-        novaLinha.appendChild(btnConcluir);
+        // novaLinha.appendChild(btnExcluir);
         novaLinha.appendChild(btnValor);
+        novaLinha.appendChild(btnDuracao);
+        novaLinha.appendChild(btnConcluir);
         tbody.appendChild(novaLinha);
 
 
-        btnExcluir.addEventListener("click", (evento)=>{
-            evento.preventDefault();
-            arrayTarefas.splice(index, 1);
-            // excluirTarefaArray(item)
-            //são dois parents pois o tr>th>button
-            evento.target.parentNode.remove();
-        })
+        // btnExcluir.addEventListener("click", (evento)=>{
+        //     evento.preventDefault();
+        //     arrayTarefas.splice(index, 1);
+        //     // excluirTarefaArray(item)
+        //     //são dois parents pois o tr>th>button
+        //     evento.target.parentNode.remove();
+        // })
+
+        //botaão concluir
         btnConcluir.addEventListener("click", (evento)=>{
             evento.preventDefault();
             //são dois parents pois o tr>th>button
             evento.target.parentNode.remove();
         })
 
-        //adicionar valor na tabela
+        //adicionar duração
+        btnDuracao.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            let duracao = prompt("Insira um valor: ")
+
+            let duracaoCelula = document.createElement("td");
+            duracaoCelula.textContent = `Duração: ${duracao}`;
+    
+            // Inserir a nova célula de valor na mesma linha
+            novaLinha.insertBefore(duracaoCelula, btnValor);
+            // novaLinha.appendChild(criaElemento(objeto.valor, "td"));
+            objeto.duracao = duracao;
+            console.log(arrayTarefas)
+        })
+
+        //adicionar valor
         btnValor.addEventListener("click", (evento)=>{
             evento.preventDefault();
-            valor = prompt("Insira um valor: ")
-            objeto.valor = valor
-            console.log(objeto.valor, index)
-            novaLinha.appendChild(criaElemento(objeto.valor, "td"));
+            let valor = prompt("Insira um valor: ")
 
+            let valorCelula = document.createElement("td");
+            valorCelula.textContent = `VALOR: ${valor}`;
+    
+            // Inserir a nova célula de valor na mesma linha
+            novaLinha.insertBefore(valorCelula, btnValor);
+            // novaLinha.appendChild(criaElemento(objeto.valor, "td"));
+
+            // arrayTarefas.push(objeto.valor)
+            objeto.valor = valor;
             // arrayTarefas.push(objeto.valor)
             console.log(arrayTarefas)
         })
