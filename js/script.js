a47a5b8f8324422318c733818d0c220569eba181
fernandakaory <rm551104@fiap.com.br>
2023-08-17 07:05:14 -0300

    adicionando campo valor

--- js/script.js
+++ js/script.js
@@ -18,40 +18,84 @@ btnAddTarefa.addEventListener("click",(evento)=>{
         descricao: descricao.value,
         autor: autor.value,
         departamento: departamento.value,
-        importancia: importancia.value
+        importancia: importancia.value,
+        valor : valor.value
     };
 
     arrayTarefas.push(novaTarefa)
     //chamando a funçao
-    criaCelula(novaTarefa)
-
-    console.log(arrayTarefas)
+    criaCelula(arrayTarefas)
 
     //chamando a função
     esvaziaCampos();
 })
 
+const btnOrdenar = document.getElementById("btnOrdenar");
+
+btnOrdenar.addEventListener("click", () => {
+    ordenarPorImportancia(arrayTarefas);
+    criaCelula(arrayTarefas);
+});
+
+
+function ordenarPorImportancia(array) {
+    array.sort((a, b) => {
+        return a.importancia - b.importancia;
+    });
+}
+
 //função para criar celula inteira de tarefa
-function criaCelula(objeto) {
-    let novaLinha = document.createElement("tr")
-    let btnExcluir = document.createElement("button");
-    btnExcluir.textContent = "Excluir"
+function criaCelula(arrayTarefas) {
+    tbody.innerHTML = ""
 
-    //usando o append direto, sem declarar uma variável para o valor 
-    novaLinha.appendChild(criaElemento(objeto.descricao,"th"))
-    novaLinha.appendChild(criaElemento(objeto.autor,"th"))
-    novaLinha.appendChild(criaElemento(objeto.departamento,"th"))
-    novaLinha.appendChild(criaElemento(objeto.importancia,"th"))
-    // novaLinha.appendChild(criaElemento("Excluir","button"))
-    novaLinha.appendChild(btnExcluir)
-    tbody.appendChild(novaLinha)
+    //o objeto e seu indice como parametro. cada objeto recebe um indeice
+    arrayTarefas.forEach((objeto, index) => {
+        let novaLinha = document.createElement("tr")
+        let btnExcluir = document.createElement("button");
+        btnExcluir.textContent = "Excluir"
+        let btnConcluir = document.createElement("button");
+        btnConcluir.textContent = "Concluído"
+        let btnValor = document.createElement("button");
+        btnValor.textContent = "Valor"
 
-    btnExcluir.addEventListener("click", (evento)=>{
-        evento.preventDefault();
-        //são dois parents pois o tr>th>button
-        evento.target.parentNode.parentNode.remove();
-    })
+        //usando o append direto, sem declarar uma variável para o valor 
+        novaLinha.appendChild(criaElemento(objeto.descricao,"td"));
+        novaLinha.appendChild(criaElemento(objeto.autor,"td"));
+        novaLinha.appendChild(criaElemento(objeto.departamento,"td"));
+        novaLinha.appendChild(criaElemento(objeto.importancia,"td"));
+        // novaLinha.appendChild(criaElemento("Excluir","button"))
+        novaLinha.appendChild(btnExcluir);
+        novaLinha.appendChild(btnConcluir);
+        novaLinha.appendChild(btnValor);
+        tbody.appendChild(novaLinha);
 
+
+        btnExcluir.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            arrayTarefas.splice(index, 1);
+            // excluirTarefaArray(item)
+            //são dois parents pois o tr>th>button
+            evento.target.parentNode.remove();
+        })
+        btnConcluir.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            //são dois parents pois o tr>th>button
+            evento.target.parentNode.remove();
+        })
+
+        //adicionar valor na tabela
+        btnValor.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            valor = prompt("Insira um valor: ")
+            objeto.valor = valor
+            console.log(objeto.valor, index)
+            novaLinha.appendChild(criaElemento(objeto.valor, "td"));
+
+            // arrayTarefas.push(objeto.valor)
+            console.log(arrayTarefas)
+        })
+})
+console.log(arrayTarefas)
 }
 
 //função para criar nova coluna
@@ -63,8 +107,14 @@ function criaElemento(valor,tag) {
 
 //funçao para limpar o campo do formulario
 function esvaziaCampos() {
-    descricao.value = ""
-    autor.value = ""
-    departamento.value = ""
-    importancia.value = ""
+    descricao.value = "";
+    autor.value = "";
+    departamento.value = "";
+    importancia.value = "";
+}
+
+function excluirTarefaArray (tarefa){
+    const indexTarefa = arrayTarefas.indexOf(tarefa)
+    array.splice(indexTarefa,1)
+    criaCelula(arrayTarefas)
 }
